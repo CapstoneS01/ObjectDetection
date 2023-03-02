ed758fc8267cccefc0f95db3f8409c76c4ec8ed2
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-03-01 21:24:02 -0500

    object detection and face recognition can work together now

--- combined/skyhunt.py
+++ combined/skyhunt.py
@@ -5,9 +5,53 @@ import face_recognition
 from djitellopy import Tello
 import face_recognition
 import time
+import torch
+
+
+class TelloYolo:
+    def __init__(self):
+        self.model = self.load_model()
+        self.classes = self.model.names
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print("\n\nDevice Used:", self.device)
+
+    def load_model(self):
+        model = torch.hub.load(
+            'yolov5', 'custom', path='yolov5/yolov5s.pt', source='local')
+        return model
+
+    def score_frame(self, frame):
+        self.model.to(self.device)
+        frame = [frame]
+        results = self.model(frame)
+
+        labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+        return labels, cord
+
+    def class_to_label(self, x):
+        return self.classes[int(x)]
+
+    def plot_boxes(self, results, frame):
+        labels, cord = results
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= 0.2:
+                x1, y1, x2, y2 = int(
+                    row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+                bgr = (0, 255, 0)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+                cv2.putText(frame, self.class_to_label(
+                    labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+
+        return frame
 
 
 def skyhunt(encodings_path, detection_method):
+    #object detection
+    detection = TelloYolo()
+
     tello = Tello()
     tello.connect()
     tello.streamoff()
@@ -22,6 +66,7 @@ def skyhunt(encodings_path, detection_method):
         frame_read = tello.get_frame_read()
         frame = frame_read.frame
         frame = cv2.flip(frame, 1)
+
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame_rgb = imutils.resize(frame, width=750)
 
@@ -55,6 +100,10 @@ def skyhunt(encodings_path, detection_method):
                         2, (255, 0, 0), 3)
             cv2.rectangle(frame, (right, top), (left, bottom),
                           (255, 0, 0), 2)
+        
+        # object detection
+        results = detection.score_frame(frame)
+        frame = detection.plot_boxes(results, frame)
 
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
@@ -69,4 +118,4 @@ def skyhunt(encodings_path, detection_method):
 
 
 if __name__ == "__main__":
-    skyhunt("/Users/faizanrasool/Desktop/school/ObjectDetection/combined/encodings.pickle", "cnn")
+    skyhunt("/Users/faizanrasool/Desktop/school/ObjectDetection/combined/encodings.pickle", "hog")
